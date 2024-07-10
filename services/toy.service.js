ddcf1ec2c4aba6925fc601ee85cad3c1df4eccaa
Mia Darvasi <miadarv@gmail.com>
2024-07-10 18:04:12 +0300

    init repo

---
+++ services/toy.service.js
@@ -0,0 +1,62 @@
+
+import fs from 'fs'
+import { utilService } from './util.service.js'
+import { loggerService } from './logger.service.js'
+
+export const toyService = {
+    query,
+    getById,
+    remove,
+    save
+}
+
+const toys = utilService.readJsonFile('data/toy.json')
+
+function query(filterBy = { name: '' , maxPrice: ''}) {
+    const regex = new RegExp(filterBy.name, 'i')
+    var toysToReturn = toys.filter(toy => regex.test(toy.name))
+    if (filterBy.maxPrice) {
+        toysToReturn = toysToReturn.filter(toy => toy.price <= filterBy.maxPrice)
+    }
+    return Promise.resolve(toysToReturn)
+}
+
+function getById(toyId) {
+    const toy = toys.find(toy => toy._id === toyId)
+    return Promise.resolve(toy)
+}
+
+function remove(toyId) {
+    const idx = toys.findIndex(toy => toy._id === toyId)
+    if (idx === -1) return Promise.reject('No Such Toy')
+    toys.splice(idx, 1)
+    return _saveToysToFile()
+}
+
+function save(toy) {
+    if (toy._id) {
+        const toyToUpdate = toys.find(currToy => currToy._id === toy._id)
+        toyToUpdate.name = toy.name
+        toyToUpdate.price = toy.price
+        toy = toyToUpdate
+    } else {
+        toy._id = utilService.makeId()
+        toys.push(toy)
+    }
+    
+    return _saveToysToFile().then(() => toy)
+}
+
+
+function _saveToysToFile() {
+    return new Promise((resolve, reject) => {
+        const data = JSON.stringify(toys, null, 4)
+        fs.writeFile('data/toy.json', data, (err) => {
+            if (err) {
+                loggerService.error('Cannot write to toys file', err)
+                return reject(err)
+            }
+            resolve()
+        })
+    })
+}
